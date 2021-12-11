cf2043bad37cf9561616608a271c3b2de8c76e2e
andreschaconr <andreschacon635@gmail.com>
2021-12-10 20:35:24 -0500

    se generan los primeros movimientos de la pieza  left and right

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let ctx; // context
 
 
 function initialize(){ // function to satar all
-    canvas= document.getElementById("canvas");
+    canvas= document.getElementById("canvas"); // call "canvas" for it ID
     ctx = canvas.getContext("2d");
     ctx.scale(20,20);  // scale the point 
     update();
@@ -29,20 +29,58 @@ piece.forEach((row, y) =>{
         if( value !== 0){
         ctx.fillStyle ="blue";
         ctx.fillRect(x + offset.x,
-                     y + offset.y ,1,1); //validation to move the piece later, and change the psition
+                     y + offset.y ,1,1); //validation to move the piece later, and change the position
     }
  });
 });
 };
 
 const player1 ={
-    position: {x:5,y:3},
+    position: {x:5,y:0}, //parameters for drawpiece function
     piece : piece,
 }
+document.addEventListener("keydown",function(keyboard){
+    if(keyboard.key== "ArrowUp"){
+        //player1.position.  arriba();
+    }
+    if(keyboard.key== "ArrowDown"){
+        player1.position.y++;
+        counter=0;
+    }
+    if(keyboard.key== "ArrowLeft"){
+        player1.position.x--;
+    }
+    if(keyboard.key== "ArrowRight"){
+        player1.position.x++;
+    }
+});
 
 
-function update(){ //this function update the board and call the fuction draw
+
+
+
+
+
+
+let counter =0;  // these variables contain the information to control the time when the piece falls
+let interval =1000; // this variable control that it's only one second
+let lastime = 0;
+
+
+function update(time = 0){ //this function update the board and call the fuction draw
+   const newtime = time - lastime;
+   lastime= time;
+
+   counter += newtime;
+   if (counter>interval){ //conditional for controle the fall of the piece every second
+       player1.position.y++;
+       counter=0;
+   }
+  
+
+
     draw();
     requestAnimationFrame(update);
+   
+}
 
-};
