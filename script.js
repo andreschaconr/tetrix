80167c139a389cefd34b858e8bba7136ba461070
andreschaconr <andreschacon635@gmail.com>
2021-12-13 21:27:35 -0500

    actualizacion de alerts con bootstrap y funcionabilidad de teclas y botones

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 
-function initialize(){ //this function star all, she is called in the html whit "onload"
-    const canvas = document.getElementById('canvas'); //call to canvas for his ID
-    const ctx = canvas.getContext('2d'); //generate context to draw on canvas
-    ctx.scale(20, 20); // scale the point 
+function initialize(){ 
+    Swal.fire({title:"Bienvenido",
+    html:`<br>
+    <strong>Controles:</strong>
+    <ul class="list-group">
+    <li class="list-group-item"> <kbd>Space</kbd><br>Pausar o reanudar </li>
+    <li class="list-group-item"> <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+    <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+    </svg><br>Rotar</li>
+    <li class="list-group-item"> <kbd>Flechas de dirección</kbd><br>Mover figura hacia esa dirección</li>
+    <li class="list-group-item"><strong>También puedes usar los botones si estás en móvil</strong></li>
+    </ul>`,
+    width:"80%" ,
+    height: "80%" ,
+    confirmButtonColor:'#5ce1e6'
+    }).then(()=>{//this function star all, she is called in the html whit "onload"
+        const canvas = document.getElementById('canvas'); //call to canvas for his ID
+        const ctx = canvas.getContext('2d'); //generate context to draw on canvas
+        ctx.scale(20, 20); // scale the point  
     
     function boardSweep() { // this function clear the rows complet and asigned  a numbre score
         let rowCount = 1;
@@ -188,11 +203,25 @@ function initialize(){ //this function star all, she is called in the html whit 
                        (player1.matrix[0].length / 2 | 0);
         if (collision(board, player1)) {
             board.forEach(row => row.fill(0));
-            player1.score = 0;
-            updateScore();
+            Swal.fire({
+                title: "GAME OVER",
+                footer:'Presiona reset para volver a jugar',
+                imageUrl :'./game over.png' ,
+                imageWidth: '300px',
+                imageAlt: 'Game over',
+                confirmButtonText:'Reset',
+                confirmButtonColor:'#5ce1e6',
+                width:"80%" ,
+                height: "80%"
+                }
+                ).then(()=>{
+                    
+                    player1.score = 0;
+                    updateScore();})
+            
+            
         }
     }
-    
     
     function playerRotate(dir) { //fuction to rotate the pieces with events
         const position = player1.position.x;
@@ -303,4 +332,4 @@ function initialize(){ //this function star all, she is called in the html whit 
     playerReset(); //new game
     updateScore(); //clar and charge score
     update(); 
-    }
+    })}
